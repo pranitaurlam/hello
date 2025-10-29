2a1b2be13378fbc735c1b5e1063c7a40fc1e8b5c
urlampranita <pranitaurlam@gmail.com>
2025-10-29 16:29:46 +0530

    Finished evenOdd function

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function add(a, b) {
 function substraction(a,b) {
     return a-b;
 }
+
+
+function evenOrOdd(num) {
+    // check data type, then proceed ahead.
+    
+    if(num % 2 === 0) {
+        return "even"
+    } else {
+        return "odd"
+    }
+}
+
+
